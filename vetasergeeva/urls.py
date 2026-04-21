3ef92c9ad3e663bbfcdef6620113eb64df322532
serv <sainthammer@icloud.com>
2026-04-21 19:46:05 +0300

    fixed

--- vetasergeeva/urls.py
+++ vetasergeeva/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("landing.urls")),
-    path("example/", include("exaplesite.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
